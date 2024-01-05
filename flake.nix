dc42233e94312694618cc3cf23078cff22f1d1ed
RegularTetragon <RegularTetragon@github.com>
2024-01-04 23:50:48 -0500

    init

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Home Manager configuration of vael";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "nixpkgs/nixos-23.05";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, nixpkgs-stable, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      overlay-stable = final: prev: {
+        stable = import nixpkgs-stable {
+          inherit system;
+	  config.allowUnfree = true;
+        };
+      };
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."vael" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ 
+	  ({ config, pkgs, ... }: {nixpkgs.overlays = [overlay-stable];}) 
+	  ./home.nix
+	];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
