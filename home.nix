1c0e26b9048253a9fb1f826d8ac455ff34ba4877
vael <RegularTetragon@github.com>
2024-08-22 16:55:15 -0400

    improve hyprland bindings. convert to master layout.

--- home.nix
+++ home.nix
@@ -25,7 +25,7 @@
       godot_4
       steamcmd
       discord-canary
-      nautilus
+      nautilus qview
       deluge wgnord 
       krita
       lmms
@@ -57,14 +57,15 @@
       waypipe wayvnc
       osslsigncode
       bambu-studio orca-slicer
-      btop ncdu
+      btop ncdu file
       brightnessctl
       hyprcursor
       thunderbird
       dosbox-x
       # trenchbroom
-      stable.yabridge stable.yabridgectl stable.winetricks stable.wineWowPackages.stable stable.corefonts
-      rofi-wayland swww waypaper grim slurp wl-clipboard dunst qt5ct networkmanagerapplet jq
+      lutris
+      stable.yabridge stable.yabridgectl stable.winetricks stable.wineWowPackages.waylandFull stable.corefonts
+      swww waypaper grim slurp wl-clipboard dunst qt5ct networkmanagerapplet jq
       # language servers and such
       nodePackages.nodejs nodePackages.coc-clangd clang-tools nil
       (retroarch.override {
@@ -76,10 +77,25 @@
         ];
       })
   ];
-
-
+  
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.rofi = {
+    enable = true;
+    terminal = "${pkgs.kitty}/bin/kitty";
+    font = "FiraCode Nerd Font 18";
+    package = pkgs.rofi-wayland;
+    theme = "catppuccin-macchiato";
+    extraConfig = {
+      display-drun="   Apps ";
+      display-run="   Run ";
+      display-window=" 󰕰  Window";
+      display-Network=" 󰤨  Network";
+      sidebar-mode=true;
+    };
+  };
+
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
@@ -89,6 +105,7 @@
         "QT_QPA_PLATFORMTHEME,qt5ct"
         "HYPRCURSOR_THEME,catppuccin-macchiato-pink-cursors"
         "HYPRCURSOR_SIZE,32"
+        "WINEPREFIX=${config.home.homeDirectory}/.wine/winecfg"
       ];
       input = {
         kb_layout = "us";
@@ -107,24 +124,24 @@
         gaps_out = 4; 
         "col.active_border" = "rgba(f5bde6ff) rgba(f5bde6ff) 45deg";
         "col.inactive_border" = "rgba(363a4fff)";
-        layout = "dwindle";
+        layout = "master";
       };
       decoration = {
         rounding = 12;
         blur = {
           enabled = true;
           size = 16;
-          passes = 4;
+          passes = 2;
           ignore_opacity = true;
         };
         inactive_opacity = 0.8;
         drop_shadow = "yes";
-        shadow_range = 4;
-        shadow_render_power = 3;
-        "col.shadow" = "rgba(1a1a1aff)";
+        shadow_range = 8;
+        shadow_render_power = 1;
+        "col.shadow" = "0xee1a1a1a";
       };
       animations = {
-        enabled = "no";
+        enabled = "yes";
         bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
         animation = [
           "windows, 1, 7, myBezier, slide"
@@ -136,6 +153,10 @@
       dwindle = {
         pseudotile = "yes";
         preserve_split = true;
+      };
+      master = {
+        orientation = "center";
+        # always_center_master = true;
       };
       gestures = {
         workspace_swipe = "on";
@@ -151,34 +172,48 @@
         "float, title:^(Picture-in-Picture)$"
         "pin, title:^(Picture-in-Picture)$"
       ];
-
+      bindr = [
+        "$mainMod, Super_L, exec, killall rofi || rofi -show drun -show-icons"
+      ];
       bind = [
-        "$mainMod, Return, exec, kitty"
         "$mainMod, C, killactive," 
         "$mainMod, Escape, exit," 
         "$mainMod, E, exec, nautilus"
         "$mainMod, F, exec, firefox"
         "$mainMod, V, togglefloating,"
-        "$mainMod, Space, exec, rofi -show drun -show-icons"
+        "$mainMod, Space, exec, kitty"
         "$mainMod, P, pseudo,"
         "$mainMod, R, togglesplit,"
         ", Print, exec, grim -g \"$(slurp -d)\" - | wl-copy"
         "SHIFT, Print, exec, grim - | wl-copy"
 
         # Move focus with mainMod + arrow keys
-        "$mainMod, H, movefocus, l"
-        "$mainMod, L, movefocus, r"
-        "$mainMod, K, movefocus, u"
-        "$mainMod, J, movefocus, d"
         "$mainMod SHIFT, H, movewindow, l"
         "$mainMod SHIFT, L, movewindow, r"
         "$mainMod SHIFT, K, movewindow, u"
         "$mainMod SHIFT, J, movewindow, d"
-        "$mainMod CTRL, H, resizeactive, -64 0"
-        "$mainMod CTRL, L, resizeactive, 64 0"
-        "$mainMod CTRL, K, resizeactive, 0 -64"
-        "$mainMod CTRL, J, resizeactive, 0 64"
 
+        # master binds
+        "$mainMod, left, movefocus, l"
+        "$mainMod, right, movefocus, r"
+        "$mainMod, up, movefocus, u"
+        "$mainMod, down, movefocus, d"
+        "$mainMod CTRL, left, movewindow, l"
+        "$mainMod CTRL, right, movewindow, r"
+        "$mainMod CTRL, up, movewindow, u"
+        "$mainMod CTRL, down, movewindow, d"
+        "$mainMod, comma, layoutmsg, addmaster"
+        "$mainMod, period, layoutmsg, removemaster"
+        "$mainMod SHIFT, left, resizeactive, -64 0"
+        "$mainMod SHIFT, right, resizeactive, 64 0"
+        "$mainMod SHIFT, up, resizeactive, 0 -64"
+        "$mainMod SHIFT, down, resizeactive, 0 64"
+        "$mainMod ALT, left,  layoutmsg, orientationleft"
+        "$mainMod ALT, right, layoutmsg, orientationright"
+        "$mainMod ALT, up,    layoutmsg, orientationtop"
+        "$mainMod ALT, down,  layoutmsg, orientationbottom"
+        "$mainMod ALT, space,  layoutmsg, orientationcenter"
+        "$mainMod, tab, workspace, e+1"
 
         "$mainMod, mouse_down, workspace, e+1"
         "$mainMod, mouse_up, workspace, e-1"
@@ -198,9 +233,6 @@
       bindle = [
         ", XF86MonBrightnessUp, exec, brightnessctl s +5%"
         ", XF86MonBrightnessDown, exec, brightnessctl s 5%-"
-        ", XF86Search, exec, launchpad"
-      ];
-      bindel = [
         ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+"
         ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
       ];
