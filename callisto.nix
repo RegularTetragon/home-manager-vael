0a677b4125c754ff01b27792e29fabb8dbc00559
vael <RegularTetragon@github.com>
2026-02-08 22:19:36 -0700

    2x scale on laptop

--- callisto.nix
+++ callisto.nix
@@ -2,7 +2,7 @@
 {
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "eDP-1,2256x1504@60,0x0,1"
+      "eDP-1,2256x1504@60,0x0,2"
 
       #      "eDP-1,1920x1080@60,0x0,1,mirror,DP-3"
       #      "DP-3,1920x1080@50,0x0,1"
